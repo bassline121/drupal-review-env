19d70b38b18faab0b5e64efe64735ad02edae42b
MASAMOTO Daichi <masamoto@dgcircus.com>
2017-05-30 15:32:07 +0900

    add

---
+++ env/default.config.sh
@@ -0,0 +1,14 @@
+
+base_drupal_root='/var/www/drupal'
+
+base_domains=(
+  'review.drupal.test'
+)
+
+git_url='https://git.example.com/drupal.git'
+
+database_prefix='review__'
+database_username='review'
+database_password='review'
+
+DRUSH='/usr/local/bin/drush'
